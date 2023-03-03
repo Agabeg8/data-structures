1b21c8357d7a853d94dfad439bf3402f58f81285
Agabeg8 <agabegh@gmail.com>
2023-03-03 10:44:39 +0100

    updated

--- 08-Search/src/main/java/LinearSearch.java
+++
@@ -1,9 +0,0 @@
-public class LinearSearch {
-    public int linearSearch(int[] array, int data){
-        for(int i=0;i< array.length;i++){
-            if (array[i]==data) return i;
-        }
-        return -1;
-    }
-
-}
