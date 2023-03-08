21728762a15c8c9b3038a0f5b34e90422f321760
Agabeg8 <agabegh@gmail.com>
2023-03-08 09:54:50 +0100

    Linked List remove

--- 02-LinkedLists/src/main/java/LinkedListApp.java
+++ 02-LinkedLists/src/main/java/LinkedListApp.java
@@ -2,14 +2,19 @@ public class LinkedListApp {
     public static void main(String[] args) {
         MySinglyLinkedList list1=new MySinglyLinkedList();
         //int[] numbers={1,1,2,2,2,3,4,4,4,4,5,6,6};
-        for(int i=0;i<10;i++) list1.add(i);
+        for(int i=0;i<11;i++) list1.add(i);
+        list1.printNodes();
+        System.out.println("Kth item from last is: "+ list1.getKthFromLast(4));
+        //System.out.println("Size before deletion is: "+ list1.size);
+
+        list1.removeKthFromLast(3);
         //System.out.println("With duplicates");
         //list1.printNodes();
         //System.out.println("Without duplicates");
         //list1.removeDuplicatesFromLinkedList();
-        list1.printNodes();
-       System.out.println("After reverse in one pass");
-       list1.reverse();
+        //list1.printNodes();
+       //System.out.println("After reverse in one pass");
+       //list1.reverse();
        list1.printNodes();
 
     }
